338da84e6d3a46c70d0dcdc004aa3c1d88d8097b
denizzsarierr <denizsarier41950@hotmail.com>
2025-01-29 19:17:21 +0100

    Bug Fixed, Login function , flash messages created

--- website/models.py
+++ website/models.py
@@ -21,6 +21,6 @@ class User(db.Model,UserMixin):
 
     id = db.Column(db.Integer,primary_key = True)
     email = db.Column(db.String(100),unique = True)
-    username = db.Columnt(db.String(25),unique = True)
+    username = db.Column(db.String(25),unique = True)
     password = db.Column(db.String(16))
     notes = db.relationship("Note")
