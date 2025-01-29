fdbf5a3cebaccef639980f93d4ca61ae3c490f32
Nurefşan Yozcu <152998601+senoralinta@users.noreply.github.com>
2025-01-29 23:50:19 +0100

    Signup function created

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import login_manager,LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,6 +11,8 @@ def create_app():
 
     app = Flask(__name__)
     app.config["SECRET_KEY"] = 'flaskgroupproject'
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
 
     from .views import views
@@ -18,6 +21,19 @@ def create_app():
     app.register_blueprint(views,url_prefix = "/")
     app.register_blueprint(auth,url_prefix = '/')
 
+    from .models import User,Note
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    #Configuration of login manager////
+    #Where do we need to go if we not logged in
+    login_manager.init_app(app)
+    #Telling which app we are using
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    create_db(app)
 
     return app
 
@@ -25,5 +41,5 @@ def create_app():
 def create_db(app):
 
     with app.app_context():
-        if not path.exists(f"/website" + {DB_NAME}):
+        if not path.exists("/website" + DB_NAME):
             db.create_all()
--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,10 @@
 #User Loggin and Signup - Authorization
 from flask import Blueprint,render_template,request,flash,redirect,url_for
-from .models import User
+from .models import User,db
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import login_user,current_user
+from flask_login import login_user,login_required,logout_user,current_user  
+from . import models
+
 
 
 auth = Blueprint("auth",__name__)
@@ -16,7 +18,7 @@ def login():
     if request.method == "POST":
 
         username = form.get('username')
-        password = form.get('password')
+        password = form.get('password1')
 
         user = User.query.filter_by(username = username).first()
 
@@ -41,10 +43,30 @@ def login():
 
 
 
-@auth.route("/sign-up",methods = ["GET","POST"])
-def signup():
+@auth.route('/sign-up',methods = ["GET","POST"])
+def sign_up():
 
-    return render_template('signup.html')
+    form = request.form
+    
+    if request.method == "POST":
+        email = form.get("email")
+        username = form.get("username")
+        password1 = form.get("password1")
+        password2 = form.get("password2")
+
+        if len(email) > 0 and len(username) > 0 and len(password1) > 0 and len(password2) > 0:
+            new_user = models.User(email=email,username=username,password = generate_password_hash(password1,method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user,remember=True)
+            flash("Account Created",category="success")
+            return redirect(url_for('views.home'))
+
+
+
+
+
+    return render_template('signup.html',user = current_user)
 
 @auth.route('/logout')
 def logout():
