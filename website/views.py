e618e61c648afbec20852f00e244d8099ee32497
Samed Yuce <rsylmzwm@gmail.com>
2025-01-30 01:52:50 +0100

    js, homepage and deletenote

--- website/views.py
+++ website/views.py
@@ -3,6 +3,7 @@ from flask import Blueprint,render_template,request,flash,redirect,url_for,jsoni
 from flask_login import current_user,login_required
 from .models import Note
 from . import models,db
+import json
 
 
 views = Blueprint("views",__name__)
@@ -25,3 +26,17 @@ def home():
             flash("Note Added",category="success")
             return redirect(url_for('views.home'))
     return render_template("home.html",user = current_user)
+
+@views.route("/delete-note",methods = ["POST"])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note["noteId"]
+    note = Note.query.get(noteId)
+
+    if note:
+
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    
+    return jsonify({})
