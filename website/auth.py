9099fb6bcd8dbc2cf445eb61b3d7cb91821cb80e
denizzsarierr <denizsarier41950@hotmail.com>
2025-02-03 18:52:23 +0100

    Final Project

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,11 @@
 #User Loggin and Signup - Authorization
-from flask import Blueprint,render_template,request,flash,redirect,url_for
-from .models import User,db,Note
+from flask import Blueprint,render_template,request,flash,redirect,url_for,Response,send_file
+from .models import User,db,Note,Img
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import login_user,current_user,login_required,logout_user
 from . import models
+from werkzeug.utils import secure_filename
+from io import BytesIO
 
 auth = Blueprint("auth",__name__)
 
@@ -82,3 +84,48 @@ def delete_account():
     db.session.query(Note).filter(Note.user_id == current_user.id).delete()
     db.session.commit()
     return redirect(url_for('auth.sign_up'))
+
+
+@auth.route('/upload',methods = ["POST"])
+@login_required
+def upload():
+
+    pic = request.files['pic']
+
+    if not pic:
+
+        return redirect(url_for('views.profile'))
+    
+    filename = secure_filename(pic.filename)
+    mimetype = pic.mimetype
+
+    img = Img(img=pic.read(),mimetype = mimetype,name = filename)
+    db.session.add(img)
+    db.session.flush()
+
+    if current_user.img:
+        old_image = current_user.img[0]
+        db.session.delete(old_image)
+
+    current_user.img = [img]
+    
+    db.session.commit()
+
+    return redirect(url_for('views.home'))
+
+
+@auth.route('/image/<int:id>')
+def get_img(id):
+
+    img = Img.query.get(id)
+
+    if img:
+    
+        image_data = BytesIO(img.img)
+        image_data.seek(0)
+        return send_file(image_data, mimetype=img.mimetype)
+    else:
+        return send_file('static/default-profile.jpg', mimetype='image/jpg')
+    
+
+
